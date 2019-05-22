edcfc2e1068ada56e3bba22dae956102174022ff
José Netto <jnetto@mineiro.org>
2019-05-22 02:33:01 -0300

    Built storybook static website

---
+++ storybook-static/precache-manifest.ad206c8a858f8140037a186984aa9b95.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5563eabf4df3252c0733a816f2e19d",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.1c9b3acc759ec427d399.bundle.js"
+  },
+  {
+    "revision": "213f54ce2883e9dc7d1d",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.1c9b3acc759ec427d399.bundle.js"
+  },
+  {
+    "url": "vendors~main.1c9b3acc759ec427d399.bundle.js"
+  }
+]);
